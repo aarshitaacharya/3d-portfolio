f5ab1d279dc545d3636a82a6aeb48e5eb6020d1a
aarshitaacharya <acharyaaarshita@gmail.com>
2024-04-19 18:35:11 +0530

    popup and homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,9 @@ import Island from '../models/Islands'
 import Sky from '../models/Sky'
 import Bird from '../models/Bird'
 import Plane from '../models/Plane'
+import HomeInfo from '../components/HomeInfo'
 
-{/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center' >
-  POPUP
-</div> */}
+
 
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
@@ -47,6 +46,11 @@ const Home = () => {
   const [planeScale, planePosition] = adjustPlaneForScreenSize();
   return (
     <section className='w-full h-screen relative'>
+
+      <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center' >
+      {currentStage && <HomeInfo currentStage = {currentStage}/>}
+    </div>
+
       <Canvas 
         className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing': 'cursor-grab'}`}
         camera={{near: 0.1, far:1000}}>
